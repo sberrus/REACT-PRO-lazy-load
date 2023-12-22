daf77db70975a09772e184c495ad6acca8621017
samuel <samuelberrus@gmail.com>
2023-12-22 01:48:39 +0100

    Componente Suspense

--- section-1/src/routes/Navigation.tsx
+++ section-1/src/routes/Navigation.tsx
@@ -6,35 +6,46 @@ import {
   Routes,
 } from "react-router-dom";
 import { routes } from "./Routes";
+import { Suspense } from "react";
 
 const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          [LOGO]
-          <ul>
-            {routes.map(({ to, name }) => (
-              <li key={to}>
-                <NavLink
-                  to={to}
-                  className={({ isActive }) => (isActive ? "nav-active" : "")}
-                >
-                  {name}
-                </NavLink>
-              </li>
+    <Suspense fallback={<span>Cargando</span>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            [LOGO]
+            <ul>
+              {routes.map(({ to, name }) => (
+                <li key={to}>
+                  <NavLink
+                    to={to}
+                    className={({ isActive }) => (isActive ? "nav-active" : "")}
+                  >
+                    {name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Routes>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
             ))}
-          </ul>
-        </nav>
-        <Routes>
-          {routes.map(({ path, Component }) => (
-            <Route key={path} path={path} element={<Component />} />
-          ))}
-          <Route path="*" element={<Navigate replace to={routes[0].to} />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+            <Route path="*" element={<Navigate replace to={routes[0].to} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   );
 };
 
 export default Navigation;
+
+
+/**
+ * Componente SUSPENSE
+ * 
+ * Este componente nos permite indicarle a React el nodo desde el cual va a trabajar con lazy load componentes. 
+ * Esto evita que salgan los errores de suspense y debemos arropar todo el BrowserRouter para que tenga efecto. 
+ */
